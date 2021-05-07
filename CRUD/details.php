549abce9db7cb317153121144917375643b8dcef
Natalia <natalia_bochenek@hotmail.com>
2021-05-07 12:46:42 +0200

    adding PHP functionality for details.php

--- CRUD/details.php
+++ CRUD/details.php
@@ -1,4 +1,54 @@
 <?php
   require_once "actions/db_connect.php";
-  
+
+  if ($_GET['id']) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM products WHERE id = {$id}" ;
+    $result = $connect->query($sql);
+    $data = $result->fetch_assoc();
+    if ($result->num_rows == 1) {
+      $locationName = $data['locationName'];
+      $price = $data['price'];
+      $descr = $data['descr'];
+      $picture = $data['picture'];
+      $longitude = $data['longitude'];
+      $latitude = $data['latitude'];
+    } else {
+        header("location: error.php");
+    }
+    $connect->close();
+  }  
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- Bootstrap, MDB stylings, Fonts, Awesome Icons -->
+  <?php require_once 'components/boot_fonts.php' ?>
+
+  <!-- adding link to CSS styles only for this project -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Details for <?php echo $locationName ;?></title>
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <img src="pictures/<?php echo $picture ;?>" class="card-img" alt="<?php echo $locationName ;?>">
+      <div class="card-img-overlay d-flex justify-content-center align-items-center ">
+        <h5 class="card-title "><?php echo $locationName ;?></h5>
+      </div>
+      <div>
+        <p class="card-text"><?php echo $descr ;?></p>
+        <p class="card-text"><?php echo $price ;?>€</p>
+      </div>
+    </div>
+  </div>
+  
+</body>
+</html>
