822c4b1a25152bda8c29d3cc19e297619e577c98
Natalia <natalia_bochenek@hotmail.com>
2021-05-07 09:28:55 +0200

    adding css & bootstrap to file

---
+++ CRUD/create.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- Bootstrap, MDB stylings, Fonts, Awesome Icons -->
+  <?php require_once 'components/boot_fonts.php' ?>
+
+  <style>
+    /* adding link to CSS styles only for this project */
+    <?php require_once 'css/style.css' ?>
+  </style>
+  
+  <title>Create Offer || Mount Everest Travel Agency</title>
+</head>
+<body>
+  
+</body>
+</html>
