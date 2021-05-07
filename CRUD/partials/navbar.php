1c2072774e38a808824ed173bc8de46f33aaab69
Natalia <natalia_bochenek@hotmail.com>
2021-05-07 20:39:51 +0200

    finish to navbar?

--- CRUD/partials/navbar.php
+++ CRUD/partials/navbar.php
@@ -1,55 +1,65 @@
-<nav class="bg-dark text-light">
-  <ul class="nav justify-content-center ">
-    <a class="navbar-brand" href="index.php">Navbar</a>
+<div class="shadow">
+  <nav class="text-light navBar p-1 d-flex flex-row justify-content-between">
+    <a class="navbar-brand text-light fs-1 ms-3" href="index.php"><i class="fab fa-earlybirds"></i> META</a>
 
-    <li class="nav-item">
-      <a class="nav-link" href="index.php">Home</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="product-list.php">Offers' List</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="create.php">Add Offer</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="bonus/webservices.php">Bonus Exercise</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="bonus/webservices.php">Bonus PHP</a>
-    </li>
-  </ul>
+    <ul class="nav align-items-center text-light w-75">
 
-  <div class="container">
-    <button class="btn btn-secondary" id="jokeBtn">Show joke</button>
-    <span class="text-light" id="jokeOutput">
-      JOKE:
-      <!-- joke content -->
-    </span>
+      <li class="nav-item">
+        <a class="nav-link text-light " href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light " href="product-list.php">Offers' List</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light " href="create.php">Add Offer</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light " href="bonus/webservices.php">Bonus Exercise</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light " href="bonus/webservices.php">Bonus PHP</a>
+      </li>
+    </ul>
+
+  </nav>
+
+  <div class="navBar pb-1 ps-3">
+      <button class="btn btn-light" id="jokeBtn">Show joke</button>
+      <span class="text-light" id="jokeOutput">
+        <!-- joke content -->
+      </span>
   </div>
+</div>
 
-  <script>
-    document.getElementById("jokeBtn").addEventListener("click", showJoke, false);
+<div class="card bg-dark text-white">
+  <img src="../pictures/haseeb-jamil-3s85IxVDyXE-unsplash.jpg" class="card-img cardImg" alt="Beautiful mountains by night">
+  <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
+    <h5 class="card-title text-center">Welcome to Mount Everest Travel Agency</h5>
+    <p class="card-text">META is your reliable parter for booking and organising hiking trips around the world.</p>
+    <a class="btn btn-light" href="#offers">See our offers</a>
+  </div>
+</div>
 
-    function showJoke(){
-      console.log('joke button works');
-      let request = new XMLHttpRequest();
-      request.open("GET", "partials/joke_api.php", true);
+<script>
+  document.getElementById("jokeBtn").addEventListener("click", showJoke, false);
 
-      request.onload = function() {
-        console.log('onload works');
-        if(this.status == 200) {
-          console.log("status 200 -- works");
-          let jokes = JSON.stringify(this.responseText);
-          let jokeOutput = `${jokes}`
-          document.getElementById("jokeOutput").innerHTML = jokeOutput;
-          ;
-        } 
-      }
+  function showJoke(){
+    console.log('joke button works');
+    let request = new XMLHttpRequest();
+    request.open("GET", "partials/joke_api.php", true);
 
-      request.send();
+    request.onload = function() {
+      console.log('onload works');
+      if(this.status == 200) {
+        console.log("status 200 -- works");
+        let jokes = JSON.stringify(this.responseText);
+        let jokeOutput = `${jokes}`
+        document.getElementById("jokeOutput").innerHTML = jokeOutput;
+        ;
+      } 
     }
 
-  </script>
+    request.send();
+  }
 
-
-</nav>
+</script>
