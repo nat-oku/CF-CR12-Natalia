f84506a94c1ef4af55a3183b42c082bd6e9a8aed
Natalia <natalia_bochenek@hotmail.com>
2021-05-07 20:49:09 +0200

    adding hero to project

---
+++ CRUD/partials/hero.php
@@ -0,0 +1,8 @@
+<div class="card bg-dark text-white">
+  <img src="../pictures/haseeb-jamil-3s85IxVDyXE-unsplash.jpg" class="card-img cardImg" alt="Beautiful mountains by night">
+  <div class="card-img-overlay d-flex flex-column justify-content-center align-items-center">
+    <h5 class="card-title text-center">Welcome to Mount Everest Travel Agency</h5>
+    <p class="card-text">META is your reliable parter for booking and organising hiking trips around the world.</p>
+    <a class="btn btn-danger" href="#offers">See our offers</a>
+  </div>
+</div>
