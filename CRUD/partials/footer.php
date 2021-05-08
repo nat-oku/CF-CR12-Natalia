8404aeb44d547d8c2fbe017358d231391691dea1
Natalia <natalia_bochenek@hotmail.com>
2021-05-08 09:28:51 +0200

    adding styles for footer

---
+++ CRUD/partials/footer.php
@@ -0,0 +1,24 @@
+<nav class="text-light navBar p-1 d-flex flex-column justify-content-center align-items-center">
+  <ul class="nav align-items-center text-light h5">
+
+    <li class="nav-item">
+      <a class="nav-link text-light " href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-light " href="product-list.php">All Offers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-light " href="create.php">Add Offer</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-light " href="bonus/webservices.php">Bonus Exercise API</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link text-light " href="bonus/webservices.php">JSON & PHP</a>
+    </li>
+  </ul>
+
+  <a class="navbar-brand text-light fs-1 ms-3" href="index.php"><i class="fab fa-earlybirds"></i> META</a>
+
+  <p class="fw-light">&#169; Natalia Bochenek CodeFactory 2021</p>
+</nav>
