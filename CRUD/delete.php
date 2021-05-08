1dd81a1979e59acd1adb0d4cb7d9b417eea358e2
Natalia <natalia_bochenek@hotmail.com>
2021-05-08 09:14:48 +0200

    adding styles to delete.php

--- CRUD/delete.php
+++ CRUD/delete.php
@@ -37,12 +37,12 @@ require_once 'actions/db_connect.php';
   <title>Delete Offer</title>
 </head>
 <body>
-  <div class="container">
+<div  class="container mt-3 mb-3"> 
     <fieldset>
-      <legend class='h2 mb-3'> Delete request <img class='img-thumbnail rounded-circle'src='pictures/<?php echo $picture ?>' alt="<?php echo $locationName ?>"></legend>
-      <h5>You have selected the data below:</h5>
+      <legend class='h2 text-danger fw-light'> Delete request <img class='img-thumbnail'src='pictures/<?php echo $picture ?>' alt="<?php echo $locationName ?>"></legend>
+      <h5 class="text-light fw-light">You have selected the data below:</h5>
 
-      <table class="table w-75 mt-3">
+      <table class="table w-75 mt-1 text-light">
         <tr>
           <td><?php echo $locationName?></td>
           <td><?php echo $price?></td>
@@ -51,13 +51,13 @@ require_once 'actions/db_connect.php';
         </tr>
       </table>
 
-      <h3 class="mb-4">Do you really want to delete this offer?</h3>
+      <h3 class="mb-4 text-danger fw-light">Do you really want to delete this offer?</h3>
 
       <form action="actions/a_delete.php" method="post">
         <input type="hidden" name="id" value="<?php echo $id ?>" />
         <input type="hidden" name="picture" value="<?php echo $picture ?>" />
         <button class="btn btn-danger"  type="submit"> Yes, delete it! </button>
-        <a href="index.php"><button class="btn btn-warning"  type="button"> No, go back! </button></a >
+        <a href="product-list.php"><button class="btn btn-outline-light" type="button"> No, go back! </button></a >
       </form>
 
     </fieldset>
